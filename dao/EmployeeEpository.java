80718ebd2a4a1fa452f503f31f0549e6122b702d
Seemamisal <102429680+Seemamisal@users.noreply.github.com>
2024-07-19 12:02:59 +0530

    Delete dao directory

--- dao/EmployeeEpository.java
+++
@@ -1,9 +0,0 @@
-package com.api.test.dao;
-
-import org.springframework.data.repository.CrudRepository;
-import com.api.test.entity.*;
-
-public interface EmployeeEpository extends CrudRepository<Employee, Integer> 
-{
-	public Employee findById(int id);
-}
